899fde128858dd10fff3355fc10cb10d2fdcab70
Cameron Seibel <cmseibel@uw.edu>
2018-05-28 19:59:11 -0700

    Factor out code that may be common among scripts

---
+++ scripts/common.R
@@ -0,0 +1,20 @@
+# Cameron Seibel
+#
+# This file will define a number of constants and functions shared between the scripts for
+# wrangling and analysis of data.
+
+# Range of the data set; although there is a better way to do this, I won't bother.
+year_range <- 1960:2017
+
+# Range of years to estimate values for; about a century of values from the present.
+estimate_range <- 2019:2119
+
+
+
+
+
+# clean_string takes a string, str, as a parameter.
+# Returns a "cleaned" version of the string, replacing problematic characters as needed.
+clean_string <- function(str){
+  return(gsub(" ", "_", str))
+}
--- scripts/data_restructure.R
+++ scripts/data_restructure.R
@@ -3,29 +3,35 @@
 # This script will refactor the given CSV files such that they will be easier to use in
 # linear modelling and nnet predictions.
 
+# Ensure that the working directory is properly set!
+source("common.R")
+
 gdp <- read.csv("../data/input/gdp_per_capita.csv", stringsAsFactors = F)
 
 countries <- gdp$Country.Name
-estimate_range <- 2019:2039
 output_df <- data.frame(year = estimate_range)
+
+# Keep track of countries that have no data such that we do not do any computation for them later
+black_list <- c()
 
 # For each country, model their GDP and estimate values for some time into the future.
 for(country in countries){
+  # Select only the columns of information, not meta-information
   data_vector <- as.numeric(as.vector(gdp_data[gdp_data$Country.Name == country, -4:-1]))
-  year_range <- 1960:2017
   select_data <- data.frame("gdp" = data_vector, "year" = year_range)
   select_data <- select_data[!is.na(select_data$gdp), ]
   # Can't model any countries whose data doesn't exist
   if(count(select_data) > 0){
-    # Clean country names for column name.
-    clean_country <- gsub(" ", "_", country)
+    clean_country <- clean_string(country)
     model <- lm(gdp ~ year, select_data)
     predictions <- data.frame(
       predict.lm(model, newdata = data.frame(year = estimate_range)), 
       estimate_range
     )
-    colnames(predictions) <- c(paste0(country, "_GDP"), "year")
+    colnames(predictions) <- c(paste0(clean_country, "_GDP"), "year")
     output_df <- merge(output_df, predictions, by = "year")
+  }else{
+    append(black_list, country)
   }
 }
 
