74598d3b4a3075246104366b6dbe86804d17ffc5
Cameron Seibel <cmseibel@uw.edu>
2018-05-30 14:48:44 -0700

    Make minor style edits

--- scripts/common.R
+++ scripts/common.R
@@ -9,7 +9,6 @@ year_range <- 1960:2017
 # Range of years to estimate values for; about a century of values from the present.
 estimate_range <- 2019:2119
 
-
 # clean_string takes a string, str, as a parameter.
 # Returns a "cleaned" version of the string, replacing problematic characters as needed.
 clean_string <- function(str){
--- scripts/current.R
+++ scripts/current.R
@@ -3,8 +3,9 @@ library("dplyr")
 library("maps")
 library("plotly")
 
+source("common.R")
 
-gdp <- read.csv("./data/input/gdp_per_capita.csv")
+gdp <- read.csv("../data/input/gdp_per_capita.csv")
 
 gdp <- gdp %>% mutate(category = ifelse(X2016 >= 12000, "Developed", "Developing"))
 
@@ -27,31 +28,27 @@ world_map <- plot_geo(gdp) %>%
     geo = g
   )
 
-
 choices <- gdp$Country.Name
 
-library(plotly)
-
-years <- c(1960:2017)
+years <- year_range
 
 gdp_t <- gdp %>% select(-Country.Code, -Indicator.Name, -Indicator.Code, -category) %>% t()
 colnames(gdp_t) = gdp_t[1, ]
 gdp_t = gdp_t[-1, ] 
 
-
 usa_data <- gdp_t[, "United States"]
 
 compare.gdp <- function(other_country){
 
-other_country_data<- gdp_t[, other_country]
-  
-p <- plot_ly(gdp, x = ~years, y = ~usa_data, name = 'United States', type = 'scatter', mode = 'lines',
-             line = list(color = 'rgb(205, 12, 24)', width = 2)) %>%
-  add_trace(y = ~other_country_data, name = ~other_country, line = list(color = 'rgb(22, 96, 167)', width = 2))  %>% 
-  layout(title = paste("Comparison of GDP/capita between United States and", other_country),
-         xaxis = list(title = "Years"),
-         yaxis = list(title = "GDP/capita", nticks = 10, tickformat = ',d'))
-p
+  other_country_data<- gdp_t[, other_country]
+    
+  p <- plot_ly(gdp, x = ~years, y = ~usa_data, name = 'United States', type = 'scatter', mode = 'lines',
+               line = list(color = 'rgb(205, 12, 24)', width = 2)) %>%
+    add_trace(y = ~other_country_data, name = ~other_country, line = list(color = 'rgb(22, 96, 167)', width = 2))  %>% 
+    layout(title = paste("Comparison of GDP/capita between United States and", other_country),
+           xaxis = list(title = "Years"),
+           yaxis = list(title = "GDP/capita", nticks = 10, tickformat = ',d'))
+  p
 }
 
 compare.gdp("India")
